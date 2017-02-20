c0ef495e2ae455951e966bedd2618bfc89878040
Martin Hylleberg <mhylle@yahoo.com>
2017-02-20 08:17:08 +0100

    score is now displaying correctly

--- src/app/scoreboard/scoreboard.component.ts
+++ src/app/scoreboard/scoreboard.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, OnDestroy} from "@angular/core";
 import {Subscription} from "rxjs";
 import {UserService} from "../services/user.service";
 import {ScoreService} from "../services/score.service";
+import {SpawnerService} from "../spawner/spawner.service";
 
 @Component({
   selector: 'scoreboard',
@@ -13,7 +14,7 @@ export class ScoreboardComponent implements OnInit,  OnDestroy {
   score: number[];
   readableScore: string;
 
-  constructor(private userService: UserService, private scoreService: ScoreService) {
+  constructor(private userService: UserService, private scoreService: ScoreService, private spawnerService: SpawnerService) {
   }
 
   ngOnInit() {
@@ -21,10 +22,17 @@ export class ScoreboardComponent implements OnInit,  OnDestroy {
     this.subscription = this.scoreService.scoreChange.subscribe(score => {
       this.score = score;
       this.readableScore = "0";
-      for (let i = this.score.length; i > 0; i--) {
+      let denominator  = "";
+      for (let i = this.score.length; i >= 0; i--) {
         if (this.score[i] > 0) {
-
+          if (denominator != "") {
+            this.readableScore +=  "." + this.score[i] +" " + denominator;
+            break;
+          }
+          denominator = this.spawnerService.units[i];
+          this.readableScore = "" + this.score[i];
         }
+
       }
     });
   }
